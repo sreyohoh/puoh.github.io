5940e3911dad3ee1586957b5af0756a9b1ed7960
Pu Oh <puoh@Pus-MacBook-Pro.local>
2024-05-25 16:37:02 +0700

    update

---
+++ jquery/custom.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+   let select = $('.box');
+   select.css('width', '100%');
+   select.css('height', '100px');
+   select.css('background', 'red');
+});
---
+++ jquery/events.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    let select = $('.box');
+    $('#btn').click(function(){
+       select.hide();
+    });
+
+});
